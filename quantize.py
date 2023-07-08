19dd1843c99c9f1dff07cf79df551e5c09e653cc
Ryan <ryan@Ryans-MacBook-Pro.local>
2023-07-08 14:51:26 -0400

    added example 32bit tensor

--- quantize.py
+++ quantize.py
@@ -1,7 +1,12 @@
 import torch
 
+X = torch.rand((5, 5))
+print("Original Tensor:")
+print(X)
+
 #where X is an unquantized tensor
 def absmax_quantize(X):
+
     # Calculate scale
     scale = 127 / torch.max(torch.abs(X))
 
@@ -12,3 +17,11 @@ def absmax_quantize(X):
     X_dequant = X_quant / scale
 
     return X_quant.to(torch.int8), X_dequant
+
+X_quant, X_dequant = absmax_quantize(X)
+
+print("\nQuantized Tensor:")
+print(X_quant)
+
+print("\nDequantized Tensor:")
+print(X_dequant)
